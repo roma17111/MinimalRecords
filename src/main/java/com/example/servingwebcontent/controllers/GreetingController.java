490b71e0a229339dff7f666f6a1bbc78c75ef0ee
roman <romanze1706@gmail.com>
2023-01-01 04:04:26 +0300

    test

---
+++ src/main/java/com/example/servingwebcontent/controllers/GreetingController.java
@@ -0,0 +1,27 @@
+package com.example.servingwebcontent.controllers;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+@Controller
+public class GreetingController {
+
+    @GetMapping("/")
+    public String greeting(Model model) {
+        model.addAttribute("name", " Roman");
+        return "home";
+    }
+    @GetMapping("/test")
+    public String start(Model model) {
+        return "greeting";
+    }
+    @GetMapping("/start")
+    public String test() {
+        return "hello";
+    }
+
+
+}
